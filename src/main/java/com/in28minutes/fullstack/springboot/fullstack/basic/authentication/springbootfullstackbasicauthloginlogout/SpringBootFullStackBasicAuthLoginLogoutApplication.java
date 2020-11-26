5e1d4bbb6586506f09c35bd8aea658f597a49337
zabcz <marc.bugmann@outlook.com>
2020-11-26 09:59:07 +0100

    init

---
+++ src/main/java/com/in28minutes/fullstack/springboot/fullstack/basic/authentication/springbootfullstackbasicauthloginlogout/SpringBootFullStackBasicAuthLoginLogoutApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.fullstack.springboot.fullstack.basic.authentication.springbootfullstackbasicauthloginlogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullStackBasicAuthLoginLogoutApplication {
+
+
+	// https://dzone.com/articles/integrating-spring-boot-and-react-with-spring-secu-1
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullStackBasicAuthLoginLogoutApplication.class, args);
+	}
+
+}
